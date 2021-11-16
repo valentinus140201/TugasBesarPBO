3782a82de2ebe08ef7a765dbacb30a2bb6ed1ce4
valentinus <valentinus.thiomarsel@gmail.com>
2021-11-16 22:26:21 +0700

    testValentinus

--- src/View/ReStockObat.java
+++ src/View/ReStockObat.java
@@ -16,6 +16,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JRootPane;
 import javax.swing.JTextField;
 import org.jdatepicker.impl.JDatePanelImpl;
 import org.jdatepicker.impl.JDatePickerImpl;
@@ -28,76 +29,91 @@ import org.jdatepicker.impl.UtilDateModel;
 
 public class ReStockObat implements ActionListener{
     private ControllerObat control = new ControllerObat();
-    JFrame frame = new JFrame("reStock obat");
+    JFrame restockObat = new JFrame("RESTOCK OBAT");
     JLabel namaObatLabel,tglBeliLabel,tglKadaluarsaLabel,StockLabel;
     UtilDateModel model1 = new UtilDateModel();
     UtilDateModel model2 = new UtilDateModel();
     Properties p = new Properties();
-    JTextField textStok,namaField;
+    JTextField textStok,namaObatField;
     JDatePanelImpl datePanel1 = new JDatePanelImpl(model1, p);
     JDatePanelImpl datePanel2 = new JDatePanelImpl(model2, p);
-    JDatePickerImpl tgl_beli = new JDatePickerImpl(datePanel1, new DateLabelFormatter());
-    JDatePickerImpl tgl_kadaluarsa = new JDatePickerImpl(datePanel2, new DateLabelFormatter());
+    JDatePickerImpl tglBeli = new JDatePickerImpl(datePanel1, new DateLabelFormatter());
+    JDatePickerImpl tglKadaluarsa = new JDatePickerImpl(datePanel2, new DateLabelFormatter());
 
     JPanel menu = new JPanel();
     JPanel isi = new JPanel();
-    JButton menu_pasien = new JButton("PASIEN");
-    JButton menu_dokter = new JButton("DOKTER");
-    JButton menu_admin = new JButton("ADMINISTRASI");
+    
+    JButton menuPasien = new JButton("PASIEN");
+    JButton menuDokter = new JButton("DOKTER");
+    JButton menuAdmin = new JButton("ADMINISTRASI");
+    
     JButton submit = new JButton("SUBMIT");
+    
     public ReStockObat(){
-        namaObatLabel = new JLabel("nama obat");
-        tglBeliLabel = new JLabel("tgl beli");
-        tglKadaluarsaLabel = new JLabel("tgl kadaluarsa");
-        StockLabel = new JLabel("stock");
+        namaObatLabel = new JLabel("Nama Obat");
+        tglBeliLabel = new JLabel("Tanggal Beli");
+        tglKadaluarsaLabel = new JLabel("Tanggal Kadaluarsa");
+        StockLabel = new JLabel("Stock");
         textStok = new JTextField();
-        namaField = new JTextField();
-        frame.setSize(1200, 700);
-        frame.setLocationRelativeTo(null);
-        frame.setLayout(null);
+        namaObatField = new JTextField();
+        
+        restockObat.setSize(1200, 620);
+        restockObat.setLocationRelativeTo(null);
+        restockObat.setLayout(null);
+        
         menu.setLayout(null);
         isi.setLayout(null);
-        menu.setBounds(10,10,200,640);
-        isi.setBounds(230,10,930,640);
-        isi.setBackground(Color.cyan);
-        menu.setBackground(Color.cyan);
-        menu_pasien.setBounds(35,200,120,50);
-        menu_dokter.setBounds(35,260,120,50);
-        menu_admin.setBounds(35,320,120,50);
         
-        namaObatLabel.setBounds(10, 10, 120, 25);
-        tglBeliLabel.setBounds(10, 40, 120, 25);
-        tglKadaluarsaLabel.setBounds(10, 70, 120, 25);
-        StockLabel.setBounds(10, 100, 120, 25);
+        menu.setBounds(10,520,1170,50);
+        isi.setBounds(10,10,1170,500);
         
-        namaField.setBounds(140, 10, 120, 25);
-        tgl_beli.setBounds(140, 40, 120, 25);
-        tgl_kadaluarsa.setBounds(140, 70, 120, 25);
-        textStok.setBounds(140, 100, 120, 25);
+        isi.setBackground(Color.ORANGE);
+        menu.setBackground(Color.ORANGE);
         
-        submit.setBounds(10, 500, 100, 25);
+        menuDokter.setBounds(320,10,90,30);
+        menuPasien.setBounds(520,10,90,30);
+        menuAdmin.setBounds(720,10,120,30);
+        
+        namaObatLabel.setBounds(40, 10, 160, 25);
+        tglBeliLabel.setBounds(40, 60, 160, 25);
+        tglKadaluarsaLabel.setBounds(40, 120, 160, 25);
+        StockLabel.setBounds(40, 170, 120, 25);
+        
+        namaObatField.setBounds(160, 10, 160, 25);
+        tglBeli.setBounds(160, 60, 160, 25);
+        tglKadaluarsa.setBounds(160, 120, 160, 25);
+        textStok.setBounds(160, 170, 160, 25);
+        
+        submit.setBounds(80, 220, 100, 25);
         
         isi.add(namaObatLabel);
         isi.add(tglBeliLabel);
         isi.add(tglKadaluarsaLabel);
         isi.add(StockLabel);
-        isi.add(namaField);
-        isi.add(tgl_beli);
-        isi.add(tgl_kadaluarsa);
+        isi.add(namaObatField);
+        isi.add(tglBeli);
+        isi.add(tglKadaluarsa);
         isi.add(textStok);
         isi.add(submit);
         
-        menu.add(menu_dokter);
-        menu.add(menu_pasien);
-        menu.add(menu_admin);
-        menu_dokter.addActionListener(this);
-        menu_pasien.addActionListener(this);
-        menu_admin.addActionListener(this);
+        menu.add(menuDokter);
+        menu.add(menuPasien);
+        menu.add(menuAdmin);
+        
+        menuDokter.addActionListener(this);
+        menuPasien.addActionListener(this);
+        menuAdmin.addActionListener(this);
+        
         submit.addActionListener(this);
-        frame.add(isi);
-        frame.add(menu);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        restockObat.add(isi);
+        restockObat.add(menu);
+        
+        restockObat.setUndecorated(true);
+        restockObat.getRootPane().setWindowDecorationStyle(JRootPane.PLAIN_DIALOG);
+        restockObat.setVisible(true);
+        restockObat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
     }
 
     @Override
@@ -106,18 +122,18 @@ public class ReStockObat implements ActionListener{
         switch(command) {
             case "PASIEN": 
                 new MenuPasien();
-                frame.setVisible(false);
+                restockObat.setVisible(false);
                 break;
             case "DOKTER":
                 new MenuDokter();
-                frame.setVisible(false);
+                restockObat.setVisible(false);
                 break;
             case "ADMINISTRASI":
                 new MenuAdmin();
-                frame.setVisible(false);
+                restockObat.setVisible(false);
                 break;
             case "SUBMIT":
-                String strnama = namaField.getText();
+                String strnama = namaObatField.getText();
                 int stok = Integer.parseInt(textStok.getText());
                 String strtglbeli = model1.getYear() + "-" + model1.getMonth() + "-" + model1.getDay();
                 String strtglkadal = model2.getYear() + "-" + model2.getMonth() + "-" + model2.getDay();
@@ -130,10 +146,10 @@ public class ReStockObat implements ActionListener{
                 obat.setTgl_kadaluarsa(strtglkadal);
                 boolean input = control.beliObat(obat);
                 if(input == true){
-                    frame.setVisible(false);
+                    restockObat.setVisible(false);
                     JOptionPane.showMessageDialog(null,"Data Sudah Di Tambah");
                     new MenuAdmin();
-                    frame.setVisible(false);
+                    restockObat.setVisible(false);
                 }
             default: 
                 break;
